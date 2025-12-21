b60b92067d09ee77513a3bc62210b101a3d2f873
Bruce Ambundo <bruceambundo@gmail.com>
2025-12-21 23:33:29 +0300

    Impact section done

--- src/pages/automation/Automation.js
+++ src/pages/automation/Automation.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Automation.css'
 import AutomationHero from './hero/AutomationHero'
+import Impact from './impact/Impact'
 
 export default function Automation() {
   return (
     <section>
-        <AutomationHero />
-        
+      <AutomationHero />
+      <Impact />
     </section>
   )
 }
