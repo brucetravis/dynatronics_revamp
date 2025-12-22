1832ca859eacf2abe1d40765bf6a987c857751bd
Bruce Ambundo <bruceambundo@gmail.com>
2025-12-22 21:13:18 +0300

    curated section ready

--- src/pages/gadgets/Gadgets.js
+++ src/pages/gadgets/Gadgets.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Gadgets.css'
 import GadgetsHero from './gadgetsHero/GadgetsHero'
+import Curated from './curated/Curated'
 
 export default function Gadgets() {
   return (
     <section>
-        <GadgetsHero />
+      <GadgetsHero />
+      <Curated />
     </section>
   )
 }
---
+++ src/pages/gadgets/curated/Curated.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Curated.css'
+import { motion } from 'framer-motion'
+
+export default function Curated() {
+  return (
+    <section
+        className='curated-section'
+    >
+        <motion.div
+            className='curated-content'
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{
+                duration: 0.8,
+                ease: 'easeInOut',
+                delay: 0.2
+            }}
+        >
+            <p className="curated-line strong">
+                We don’t sell everything.
+            </p>
+
+            <p className="curated-line">
+                We curate rare technology defined by design, intention, and intelligence.
+            </p>
+
+            <p className="curated-line muted">
+                Every gadget is selected because it stands apart — not because it sells fast.
+            </p>
+        </motion.div>
+
+    </section>
+  )
+}
