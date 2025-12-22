1bf01587dd0b11e96210bd60e0c2bc2f038b9468
Bruce Ambundo <bruceambundo@gmail.com>
2025-12-22 21:57:35 +0300

    FeaturedGadgets section ready

--- src/pages/gadgets/Gadgets.js
+++ src/pages/gadgets/Gadgets.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Gadgets.css'
 import GadgetsHero from './gadgetsHero/GadgetsHero'
 import Curated from './curated/Curated'
+import FeaturedGadgets from './featuredGadgets/FeaturedGadgets'
 
 export default function Gadgets() {
   return (
     <section>
       <GadgetsHero />
       <Curated />
+      <FeaturedGadgets />
     </section>
   )
 }
