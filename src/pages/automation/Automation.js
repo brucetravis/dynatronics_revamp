3893844f208148265a1966f092230c43d93d9512
Bruce Ambundo <bruceambundo@gmail.com>
2025-12-22 23:00:37 +0300

    Automation scroll added

--- src/pages/automation/Automation.js
+++ src/pages/automation/Automation.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Automation.css'
 import AutomationHero from './hero/AutomationHero'
 import Impact from './impact/Impact'
@@ -6,6 +6,15 @@ import Industry from './industry/Industry'
 import WhyUs from './whyus/WhyUs'
 
 export default function Automation() {
+  
+  // useEffect to scroll to the top of the screen on mount
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+    }, 0)
+
+  }, []) // empty dependency array
+  
   return (
     <section>
       <AutomationHero />
