4fda7edd3fe50cd34d06f755d1973cb9f7983ab8
Bruce Ambundo <bruceambundo@gmail.com>
2025-12-22 22:54:12 +0300

    scrolling on mount added

--- src/pages/gadgets/Gadgets.js
+++ src/pages/gadgets/Gadgets.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Gadgets.css'
-import GadgetsHero from './gadgetsHero/GadgetsHero'
-import Curated from './curated/Curated'
-import FeaturedGadgets from './featuredGadgets/FeaturedGadgets'
-import Intelligence from '../intelligence/Intelligence'
 import Explore from './explore/Explore'
+import Curated from './curated/Curated'
+import GadgetsHero from './gadgetsHero/GadgetsHero'
+import Intelligence from '../intelligence/Intelligence'
+import FeaturedGadgets from './featuredGadgets/FeaturedGadgets'
 
 export default function Gadgets() {
+
+  // useEffect to scroll to the top of the page on mount
+  useEffect(() => {
+    
+    // Ensure the scroll happens after render
+    setTimeout(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+    }, 0)
+    
+  }, []) // empty dependency array
+
   return (
     <section>
       <GadgetsHero />
